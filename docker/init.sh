339769b209e4b0ad504ddb9c4e1f5614bafd3c16
dbierer <doug@unlikelysource.com>
2020-06-10 14:42:35 +0700

    2020-06-10 DB: updated docker scripts

--- docker/init.sh
+++ docker/init.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 echo "ServerName jumpstart" >> /etc/httpd/httpd.conf
-echo "Restoring application ..."
-cd /srv
-git clone https://github.com/phpcl/phpcl_jumpstart_lfphp jumpstart
 cd /srv/jumpstart/ip_demo
-if [ -f /srv/jumpstart/ip_demo/composer.phar ]
-then
-    php composer.phar self-update
-else
-    wget https://getcomposer.org/download/1.10.7/composer.phar
-fi
-if [ -f /srv/jumpstart/ip_demo/composer.lock ]
-then
-    rm /srv/jumpstart/ip_demo/composer.lock
-fi
+php composer.phar self-update
+rm -f /srv/jumpstart/ip_demo/composer.lock
 php composer.phar install
 echo "Restoring database ..."
 /etc/init.d/mysql start
@@ -24,11 +13,11 @@ mysql -uroot -v -e "GRANT ALL PRIVILEGES ON *.* TO 'jumpstart'@'localhost';"
 mysql -uroot -v -e "FLUSH PRIVILEGES;"
 mysql -uroot -v -e "SOURCE /srv/jumpstart/sample_data/jumpstart.sql;" jumpstart
 echo "Configuring Apache ... "
-if [ -f /srv/www ]
+if [ -d /srv/www ]
 then
-  mv /srv/www /srv/www_old
+  mv -f /srv/www /srv/www_old
 fi
-ln -s -v /srv/jumpstart/ip_demo/public /srv/www
+ln -sv /srv/jumpstart/ip_demo/public /srv/www
 chown apache:apache /srv/www
 chown -R apache:apache /srv/jumpstart/ip_demo
 chmod -R 775 /srv/jumpstart/ip_demo
