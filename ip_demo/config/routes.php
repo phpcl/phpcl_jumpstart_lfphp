bfce85b1c523343a77a0d75a52a5ce73253232bc
dbierer <doug@unlikelysource.com>
2020-06-10 15:03:45 +0700

    2020-06-10 DB: updated demo application code

--- ip_demo/config/routes.php
+++ ip_demo/config/routes.php
@@ -36,4 +36,5 @@ return static function (Application $app, MiddlewareFactory $factory, ContainerI
     $app->get('/', App\Handler\HomePageHandler::class, 'home');
     $app->get('/api/ping', App\Handler\PingHandler::class, 'api.ping');
     $app->get('/ip', IpWhat\Handler\IpHandler::class, 'ipwhat');
+    $app->post('/ip', IpWhat\Handler\IpHandler::class, 'ipwhat');
 };
